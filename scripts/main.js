90a786b895f7a2b5603d21d9956b8b7bbc3cb243
Amber Oglesby <aoglesby5795@gmail.com>
2023-01-25 00:10:20 -0500

    handlebars attempt

---
+++ scripts/main.js
@@ -0,0 +1,10 @@
+////const etsyData = document.getElementById("entry-template").innerHTML;
+//const template = Handlebars.compile(etsyData);
+
+const dataEntry = {
+  results: {},
+};
+
+$(document).ready(function(){
+    let template= $("entry-template").html
+}
